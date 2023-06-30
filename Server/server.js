076a8ec3e38be7b4317fb7555fabab5af2951c6d
Owais Nazir Dar <63143570+owaisnazir110928@users.noreply.github.com>
2023-06-30 05:55:24 +0530

    ..

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
-require("dotenv").config({ path: ".env.local" });
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 4000;
